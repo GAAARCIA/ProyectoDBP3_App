3749f6c8dc64ee7763f426ddaee8cdf9b30e54b1
Angelica Garcia <a329528@uach.mx>
2021-05-27 13:47:49 -0600

    Se inicia proyecto

---
+++ app/src/main/java/com/example/prueba1/CV.java
@@ -0,0 +1,60 @@
+package com.example.prueba1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.prueba1.modules.Datos;
+import com.google.gson.Gson;
+
+public class CV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cv);
+
+        TextView nombre = findViewById(R.id.nombre);
+        TextView bio = findViewById(R.id.bio);
+        TextView educacion = findViewById(R.id.educacion);
+        TextView habilidades = findViewById(R.id.habilidades);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Datos comoyoquiera = new Gson().fromJson(response.toString(), Datos.class);
+                        nombre.setText(comoyoquiera.nombre);
+                        bio.setText(comoyoquiera.bio);
+                        educacion.setText(comoyoquiera.educacion);
+                        habilidades.setText(comoyoquiera.habilidades);
+
+                    }
+
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("server-request-error", error.getMessage());
+            }
+
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+}
---
+++ app/src/main/java/com/example/prueba1/modules/Datos.java
@@ -0,0 +1,10 @@
+package com.example.prueba1.modules;
+
+public class Datos {
+
+    public String nombre;
+    public String bio;
+    public String educacion;
+    public String habilidades;
+
+}
